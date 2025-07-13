fb15e6e466b0f543c705dfae51fe8f722af3ef78
nandhu sivadas <nandhucsivadas@gmail.com>
2025-07-13 08:52:33 -0800

    initial

---
+++ HackerRank/FunnyString.py
@@ -0,0 +1,8 @@
+def funnyString(s):
+    r = s[::-1]  
+    for i in range(1, len(s)):
+        diff1 = abs(ord(s[i]) - ord(s[i - 1]))
+        diff2 = abs(ord(r[i]) - ord(r[i - 1]))
+        if diff1 != diff2:
+            return "Not Funny"
+    return "Funny"
