08114ac927da6329ab3ae321cd12316d14fd67ac
nandhu sivadas <nandhucsivadas@gmail.com>
2025-07-14 08:52:52 -0800

    initial

---
+++ HackerRank/InsertionSortPart1.py
@@ -0,0 +1,13 @@
+def insertionSort1(n, arr):
+    # Write your code here
+       
+    key = arr[-1] 
+    i = n - 2       
+
+    while i >= 0 and arr[i] > key:
+        arr[i + 1] = arr[i]  
+        print(*arr)                 
+        i -= 1
+
+    arr[i + 1] = key        
+    print(*arr)              
--- HackerRank/IntroToTutorialChallenges.py
+++ HackerRank/IntroToTutorialChallenges.py
@@ -2,5 +2,4 @@ def introTutorial(V, arr):
     # Write your code here
     for i in range(len(arr)):
         if arr[i]==V:
-            return arr.index(arr[i])
-            
+            return i
